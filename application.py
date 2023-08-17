58dc2e7cae886612b6bcbdfb4676651d7bda70fb
daksh7 <dakshsethi07@gmail.com>
2023-08-17 12:39:15 +0530

    first commit

--- application.py
+++ application.py
@@ -71,7 +71,7 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
+            client = pymongo.MongoClient("mongodb+srv://dakshsethi24685:dakshsethi@cluster0.mt1aniw.mongodb.net/?retryWrites=true&w=majority")
             db = client['review_scrap']
             review_col = db['review_scrap_data']
             review_col.insert_many(reviews)
@@ -85,5 +85,5 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
-	#app.run(debug=True)
+    app.run(host="0.0.0.0")
+	
